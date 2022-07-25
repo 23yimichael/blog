e1ea62f80da2a9123d4bc8cc2d658ce5348a320a
23yimichael <23yimichael@gmail.com>
2022-07-25 15:45:14 -0700

    Added music header

--- web/pages/music.tsx
+++ web/pages/music.tsx
@@ -3,7 +3,9 @@ import Layout from "../components/Layout";
 const Music = () => {
   return (
     <Layout>
-      <div className="flex h-screen items-center justify-center">Music</div>
+      <div className="flex justify-center font-poppins font-semibold 2xl:text-5xl xl:text-3xl lg:text-xl mt-12">
+        Music
+      </div>
     </Layout>
   );
 };
